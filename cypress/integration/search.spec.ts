a4cdd08068bb1b99943fc9558090fa3b07a99fa8
eliasjz36 <bseliasjimenez14@gmail.com>
2022-06-05 05:46:01 -0300

    test: add a test for error message and refactor previous tests

--- cypress/integration/search.spec.ts
+++ cypress/integration/search.spec.ts
@@ -27,28 +27,51 @@ describe('Search', () => {
     cy.visit('/');
   });
 
-  it('empty search should display no results', () => {
-    cy.get('[data-testid="search-input"]').type('{enter}');
-    cy.get('[data-testid="search-results"]').should('not.exist');
-  });
+  it('should search title and display results with mock', () => {
+    cy.intercept('GET', 'https://imdb-api.com/en/API/Search/k_s01z1s78/t', {
+      fixture: 'titles.json',
+    }).as('search');
 
-  it.only('should search title and display results', () => {
-    cy.get('[data-testid="search-input"]').type('test');
-    cy.get('[data-testid="search-input"]').should('have.value', 'test');
-    cy.get('[data-testid="search-results"]').should('be.visible');
-    cy.get('[data-testid="search-result"]')
+    cy.get('[data-testid="search-input"]').type('t');
+    cy.get('[data-testid="search-input"]').should('have.value', 't');
+    cy.url().should('contain', '/?q=t');
+
+    cy.wait('@search');
+
+    cy.get('[data-testid="search-results"]')
       .should('be.visible')
-      .first()
-      .find('h3')
-      .should('include.text', 'test', { matchCase: false });
+      .within(() => {
+        cy.get('[data-testid="search-result"]')
+          .should('be.visible')
+          .within(() => {
+            cy.get('h3').should('contain', 't', { matchCase: false });
+          });
+      });
   });
 
   it('search does not match title and displays info message', () => {
-    cy.get('[data-testid="search-input"]').type('@#$%^&*');
-    cy.get('[data-testid="search-input"]').should('have.value', '@#$%^&*');
+    cy.visit('http://localhost:3000/?q=***');
+    cy.url().should('contain', '/?q=***');
+
     cy.get('[data-testid="search-not-found"]')
       .should('be.visible')
-      .should('contain', `Your search for "@#$%^&*" did not have any matches`);
+      .should('contain', `Your search for "***" did not have any matches`);
+  });
+
+  it('should display error message when search fails', () => {
+    cy.intercept('GET', 'https://imdb-api.com/en/API/Search/k_s01z1s78/t', {
+      statusCode: 500,
+    }).as('search');
+
+    cy.get('[data-testid="search-input"]').type('t');
+    cy.get('[data-testid="search-input"]').should('have.value', 't');
+    cy.url().should('contain', '/?q=t');
+
+    cy.wait('@search');
+
+    cy.get('[role="alert"]')
+      .should('be.visible')
+      .should('contain', 'Something went wrong, please try again');
   });
 });
 
